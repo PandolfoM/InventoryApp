86a5d90c16d1dda5f7fac7fd28249ad1a3f16521
Matthew Pandolfo <matt@pandolfo.com>
2023-02-07 21:27:38 -0500

    added time

--- InventoryApp/Components/RecentlyAdded.swift
+++ InventoryApp/Components/RecentlyAdded.swift
@@ -26,16 +26,25 @@ struct RecentlyAdded: View {
       ForEach(items.prefix(5), id: \.self) { item in
         HStack {
           VStack {
-            Text("x\(item.count)")
-              .foregroundColor(.blue)
-              .frame(width: 170, alignment: .leading)
-              .font(.subheadline)
+            HStack {
+              Text("x\(item.count)")
+                .foregroundColor(.gray)
+                .font(.subheadline)
+              Text("\(item.origin?.wrappedName ?? "Unknown")")
+                .foregroundColor(.gray)
+                .font(.subheadline)
+
+            }.frame(maxWidth: 170, alignment: .leading)
             Spacer()
             Text(item.name ?? "Unknown")
-              .frame(width: 170, alignment: .leading)
               .font(.headline)
+              .frame(width: 170, alignment: .leading)
             Spacer()
-          }
+            timeCheck(item: item)
+              .foregroundColor(.gray)
+              .font(.subheadline)
+              .frame(width: 170, alignment: .leading)
+          }.frame(width: 170, alignment: .leading)
           Spacer()
           if item.image != nil {
             Image(uiImage: UIImage(data: item.image!)!)
@@ -49,6 +58,38 @@ struct RecentlyAdded: View {
           }
         }
       }
+    }
+  }
+
+  func timeCheck(item: Item) -> Text {
+    let dateDiff = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: item.date, to: Date())
+    let minute = dateDiff.minute ?? 0
+    let hour = dateDiff.hour ?? 0
+    let day = dateDiff.day ?? 0
+    let month = dateDiff.month ?? 0
+    let year = dateDiff.year ?? 0
+    if minute < 1 {
+      return Text("Now")
+    } else if minute == 1 {
+      return Text("1 Minute ago")
+    } else if minute > 1 {
+      return Text("\(minute) Minutes ago")
+    } else if hour == 1 {
+      return Text("1 Hour ago")
+    } else if hour > 1 {
+      return Text("\(hour) Hours ago")
+    } else if day == 1 {
+      return Text("1 Day ago")
+    } else if day > 1 {
+      return Text("\(day) Days ago")
+    } else if month == 1 {
+      return Text("1 Month ago")
+    } else if month > 1 {
+      return Text("\(month) Months ago")
+    } else if year == 1 {
+      return Text("1 Year ago")
+    } else {
+      return Text("\(year) Years ago")
     }
   }
 }
